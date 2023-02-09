26fa056048d19ca201e25efea52f16e19a0f651d
Apricosma <113317992+Apricosma@users.noreply.github.com>
2023-02-09 16:31:58 -0600

    Add comment to rating

--- Models/Rating.cs
+++ Models/Rating.cs
@@ -8,11 +8,36 @@
 
 		public Movie Movie { get; set; }
 
+		private string? _userComment;
+
+		public string? UserComment {
+			get { return _userComment; } 
+			set
+			{
+				if (value.Length > 2 && value.Length < 250)
+				{
+					_userComment = value;
+				} else
+				{
+					throw new ArgumentException("Comments must be between 2 and 250 characters");
+				}
+			}
+		}
+
 		public Rating(double rating, User user, Movie movie)
 		{
 			_userRating = rating;
 			User = user;
 			Movie = movie;
+			UserComment = null;
+		}
+
+		public Rating(double rating, User user, Movie movie, string? comment)
+		{
+			_userRating = rating;
+			User = user;
+			Movie = movie;
+			UserComment = comment;
 		}
 	}
 }
